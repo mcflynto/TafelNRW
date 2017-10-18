be61b8711e42154e69459cba82b721e92f4f144c
phDoll <philipp.doll@9elements.com>
2017-10-18 13:52:33 +0200

    change migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018102337) do
+ActiveRecord::Schema.define(version: 20171018114857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "adress", force: :cascade do |t|
+  create_table "adresses", force: :cascade do |t|
     t.string "street"
     t.string "city"
     t.string "phone"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20171018102337) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "doation", force: :cascade do |t|
+  create_table "doations", force: :cascade do |t|
     t.bigint "donator_id"
     t.bigint "transporter_id"
     t.string "food"
@@ -35,43 +35,43 @@ ActiveRecord::Schema.define(version: 20171018102337) do
     t.string "delivery_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["donator_id"], name: "index_doation_on_donator_id"
-    t.index ["transporter_id"], name: "index_doation_on_transporter_id"
+    t.index ["donator_id"], name: "index_doations_on_donator_id"
+    t.index ["transporter_id"], name: "index_doations_on_transporter_id"
   end
 
-  create_table "donations_tafel", id: false, force: :cascade do |t|
+  create_table "donations_tafels", id: false, force: :cascade do |t|
     t.bigint "donations_id"
     t.bigint "tafel_id"
-    t.index ["donations_id"], name: "index_donations_tafel_on_donations_id"
-    t.index ["tafel_id"], name: "index_donations_tafel_on_tafel_id"
+    t.index ["donations_id"], name: "index_donations_tafels_on_donations_id"
+    t.index ["tafel_id"], name: "index_donations_tafels_on_tafel_id"
   end
 
-  create_table "donator", force: :cascade do |t|
+  create_table "donators", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "tafel", force: :cascade do |t|
+  create_table "tafels", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "transporter", force: :cascade do |t|
+  create_table "transporters", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "transporter_tafel", id: false, force: :cascade do |t|
+  create_table "transpoters_tafels", id: false, force: :cascade do |t|
     t.bigint "transporter_id"
     t.bigint "tafel_id"
-    t.index ["tafel_id"], name: "index_transporter_tafel_on_tafel_id"
-    t.index ["transporter_id"], name: "index_transporter_tafel_on_transporter_id"
+    t.index ["tafel_id"], name: "index_transpoters_tafels_on_tafel_id"
+    t.index ["transporter_id"], name: "index_transpoters_tafels_on_transporter_id"
   end
 
   create_table "users", force: :cascade do |t|
