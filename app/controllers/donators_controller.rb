f3606e4760d8d706c6a8c7f6dc19972c30880971
julian <julian.treutler@gmail.com>
2017-10-20 11:56:31 +0200

    donator form

--- app/controllers/donators_controller.rb
+++ app/controllers/donators_controller.rb
@@ -2,6 +2,7 @@
 class DonatorsController < ApplicationController
   def new
     @donator = Donator.new
+    @donation = Donation.new
   end
 
   def create
@@ -19,7 +20,13 @@ class DonatorsController < ApplicationController
   private
 
   def donator_params
-    params.require(:donator).permit(:name, :email, address_attributes:
-                                    %i[street house_number city plz phone])
+    params.require(:donator).permit(
+      :name,
+      :email,
+      { address_attributes:
+        %i[street house_number city plz phone] },
+      { donations_attributes:
+        %i[food amount unit] }
+    )
   end
 end
--- app/models/donator.rb
+++ app/models/donator.rb
@@ -3,4 +3,5 @@ class Donator < ApplicationRecord
   has_one :address, as: :addressable
   accepts_nested_attributes_for :address
   has_many :donations
+  accepts_nested_attributes_for :donations
 end
