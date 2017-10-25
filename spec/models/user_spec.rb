585e6b4fbc76e373974fb911c008766b4e543676
julian <julian.treutler@gmail.com>
2017-10-25 18:15:04 +0200

    spec fixes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe do
-
   let(:organization) { organization = create(:organization) }
   let(:user) { user = create(:user, organization: organization) }
   let(:donator) { donator = create(:donator) }
@@ -9,8 +8,7 @@ RSpec.describe do
   let(:donation) { donation = create(:donation, donator: donator, transporter: transporter) }
   let(:share) { share = create(:share, donation: donation, organization: organization) }
 
-  it "test factory setup" do
+  it 'should test factory setup' do
     expect(donation.amount).to eql(10)
   end
-
 end
