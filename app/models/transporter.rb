0921d863e7337a36459e7c04b5f035d0e4ca8941
julian <julian.treutler@gmail.com>
2017-10-25 15:55:40 +0200

    Transporter authentication

--- app/models/transporter.rb
+++ app/models/transporter.rb
@@ -1,7 +1,16 @@
+# Transporter Model
 class Transporter < ApplicationRecord
   has_many :donations
   has_one :address, as: :addressable
   accepts_nested_attributes_for :address
 
   validates_presence_of :name, :email
+
+  def create_transporter_hash
+    self.transporter_hash = loop do
+      transporter_hash = SecureRandom.urlsafe_base64(nil, false)
+      break transporter_hash unless
+        Transporter.exists?(transporter_hash: transporter_hash)
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024114809) do
+ActiveRecord::Schema.define(version: 20171025113959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20171024114809) do
     t.string "expiry_date"
     t.boolean "ordered"
     t.string "delivery_date"
+    t.integer "ordered_amount", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "confirmed"
@@ -75,6 +76,7 @@ ActiveRecord::Schema.define(version: 20171024114809) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "transporter_hash"
   end
 
   create_table "users", force: :cascade do |t|
