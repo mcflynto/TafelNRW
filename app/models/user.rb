652034ea752124ea35904c9925360684688e6ab1
phDoll <philipp.doll@9elements.com>
2017-10-26 16:45:53 +0200

    add regex for mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   before_create :setup_activation
   belongs_to :organization
   validates_presence_of :name, :email ,:organization
+  validates_format_of :email, :with => /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :email, uniqueness: true
   validates :password, :presence => true, :confirmation => true, :on => :update
