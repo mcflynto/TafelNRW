2949b50f64b6c9e42020a9cf87da5c23ace77c95
phDoll <philipp.doll@9elements.com>
2017-11-02 11:29:17 +0100

    add letter opener gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,19 +27,20 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host:'localhost', port: '3000' }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'localhost:3000',
-    user_name: ENV['MAIL'],
-    password: ENV['PASSWORD'],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = { host:'localhost', port: '3000' }
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   domain: 'localhost:3000',
+  #   user_name: ENV['MAIL'],
+  #   password: ENV['PASSWORD'],
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true
+  # }
+  config.action_mailer.delivery_method = :letter_opener
 
   config.action_mailer.perform_caching = false
 
